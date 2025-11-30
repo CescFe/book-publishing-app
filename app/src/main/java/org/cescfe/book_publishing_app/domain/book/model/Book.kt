25a3068f5987e6d00a44bb7b543f49d55c982901
FrancescFe <fraferru@gmail.com>
2025-11-30 19:46:03 +0100

    Feat(APP-007): add book domain layer

---
+++ app/src/main/java/org/cescfe/book_publishing_app/domain/book/model/Book.kt
@@ -0,0 +1,10 @@
+package org.cescfe.book_publishing_app.domain.book.model
+
+data class Book(
+    val id: String,
+    val title: String,
+    val author: String,
+    val collection: String?,
+    val finalPrice: Double,
+    val isbn: String
+)
