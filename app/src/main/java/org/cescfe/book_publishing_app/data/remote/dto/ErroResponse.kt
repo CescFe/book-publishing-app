82b88e5c9d80fbfe70c64ee76711bf3b95eff556
FrancescFe <fraferru@gmail.com>
2025-11-30 11:24:51 +0100

    Feat(APP-005): apply spotless

--- app/src/main/java/org/cescfe/book_publishing_app/data/remote/dto/ErroResponse.kt
+++
@@ -1,8 +0,0 @@
-package org.cescfe.book_publishing_app.data.remote.dto
-
-data class ErrorResponse(
-    val status: Int,
-    val error: String,
-    val message: String,
-    val code: String? = null
-)
---
+++ app/src/main/java/org/cescfe/book_publishing_app/data/remote/dto/ErrorResponse.kt
@@ -0,0 +1,3 @@
+package org.cescfe.book_publishing_app.data.remote.dto
+
+data class ErrorResponse(val status: Int, val error: String, val message: String, val code: String? = null)
