26cde96dfd9feb7480aabc5438d2d0eda56d83f5
FrancescFe <fraferru@gmail.com>
2025-12-18 12:22:21 +0100

    Feat(APP-046): adjust CreateBottomBar

--- app/src/main/java/org/cescfe/book_publishing_app/ui/shared/navigation/CreateBottomBar.kt
+++ app/src/main/java/org/cescfe/book_publishing_app/ui/shared/navigation/CreateBottomBar.kt
@@ -1,15 +1,13 @@
 package org.cescfe.book_publishing_app.ui.shared.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,24 +15,26 @@ import org.cescfe.book_publishing_app.R
 import org.cescfe.book_publishing_app.ui.theme.BookpublishingappTheme
 
 @Composable
-fun CreateBottomBar(modifier: Modifier = Modifier, onSaveClick: () -> Unit = {}) {
-    Surface(
+fun CreateBottomBar(
+    modifier: Modifier = Modifier,
+    onSaveClick: () -> Unit = {}
+) {
+    NavigationBar(
         modifier = modifier.testTag("create_bottom_bar"),
-        tonalElevation = 3.dp
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Button(
-                onClick = onSaveClick,
-                modifier = Modifier.testTag("save_button")
-            ) {
-                Text(text = stringResource(R.string.action_save))
+        NavigationBarItem(
+            selected = false,
+            onClick = onSaveClick,
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_save),
+                    contentDescription = stringResource(R.string.action_save),
+                    modifier = Modifier
+                        .size(29.dp)
+                        .testTag("save_button")
+                )
             }
-        }
+        )
     }
 }
 
