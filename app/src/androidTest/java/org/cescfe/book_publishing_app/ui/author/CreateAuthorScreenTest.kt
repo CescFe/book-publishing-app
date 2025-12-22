d8b18a71532b167db8ed00e527978c0cda52ff52
FrancescFe <fraferru@gmail.com>
2025-12-22 17:39:42 +0100

    Feat(APP-054): adjust instrumented tests

--- app/src/androidTest/java/org/cescfe/book_publishing_app/ui/author/CreateAuthorScreenTest.kt
+++ app/src/androidTest/java/org/cescfe/book_publishing_app/ui/author/CreateAuthorScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import org.cescfe.book_publishing_app.ui.theme.BookpublishingappTheme
 import org.junit.Rule
 import org.junit.Test
@@ -21,21 +22,59 @@ class CreateAuthorScreenTest {
             }
         }
 
-        composeTestRule.onNodeWithTag("create_author_top_bar").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("back_button").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("create_bottom_bar").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("save_button").assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("create_author_top_bar")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("back_button")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("create_bottom_bar")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("save_button")
+            .assertIsDisplayed()
     }
 
     @Test
-    fun createAuthorScreen_saveButtonClick_showsConfirmationDialog() {
+    fun createAuthorScreen_validForm_saveClick_showsConfirmationDialog() {
         composeTestRule.setContent {
             BookpublishingappTheme {
                 CreateAuthorScreen()
             }
         }
 
-        composeTestRule.onNodeWithTag("save_button").performClick()
-        composeTestRule.onNodeWithTag("confirmation_dialog").assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("full_name_field")
+            .performTextInput("J. R. R. Tolkien")
+
+        composeTestRule
+            .onNodeWithTag("biography_field")
+            .performTextInput("English writer")
+
+        composeTestRule
+            .onNodeWithTag("save_button")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithTag("confirmation_dialog")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun createAuthorScreen_invalidForm_saveClick_doesNotShowConfirmationDialog() {
+        composeTestRule.setContent {
+            BookpublishingappTheme {
+                CreateAuthorScreen()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag("save_button")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithTag("confirmation_dialog")
+            .assertDoesNotExist()
     }
 }
