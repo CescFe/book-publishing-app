46bf6aed385c5993103c0f17a0453ffdf3e1b2cc
FrancescFe <fraferru@gmail.com>
2025-12-24 12:34:42 +0100

    Feat(APP-060): display Not Implemented message

--- app/src/main/java/org/cescfe/book_publishing_app/ui/book/BookScreen.kt
+++ app/src/main/java/org/cescfe/book_publishing_app/ui/book/BookScreen.kt
@@ -1,22 +1,26 @@
 package org.cescfe.book_publishing_app.ui.book
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import org.cescfe.book_publishing_app.R
-import org.cescfe.book_publishing_app.ui.shared.components.LoadingState
 import org.cescfe.book_publishing_app.ui.shared.navigation.DetailActionsBottomBar
 import org.cescfe.book_publishing_app.ui.theme.BookpublishingappTheme
 
@@ -75,8 +79,19 @@ internal fun BookScreenContent(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            // TODO: Placeholder content - will be replaced with actual data fetching
-            LoadingState()
+            // TODO: Implement Book Details Screen
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Not Implemented yet",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
