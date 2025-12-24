ce31c756aa9cce9fa4e8a5ac6e20eb3092683daa
FrancescFe <fraferru@gmail.com>
2025-12-24 12:55:53 +0100

    Feat(APP-061): add enums

---
+++ app/src/main/java/org/cescfe/book_publishing_app/domain/book/model/enums/Status.kt
@@ -0,0 +1,8 @@
+package org.cescfe.book_publishing_app.domain.book.model.enums
+
+enum class Status {
+    DRAFT,
+    PUBLISHED,
+    OUT_OF_PRINT,
+    DISCONTINUED
+}
---
+++ app/src/main/java/org/cescfe/book_publishing_app/domain/shared/enums/ReadingLevel.kt
@@ -0,0 +1,7 @@
+package org.cescfe.book_publishing_app.domain.shared.enums
+
+enum class ReadingLevel {
+    CHILDREN,
+    YOUNG_ADULT,
+    ADULT
+}
