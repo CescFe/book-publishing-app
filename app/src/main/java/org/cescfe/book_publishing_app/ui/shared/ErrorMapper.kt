6e8e24efb738e31558ec052aa2458dd6d6a125d3
FrancescFe <fraferru@gmail.com>
2026-01-02 13:47:40 +0100

    Feat(APP-073): handle 403 forbidden error

--- app/src/main/java/org/cescfe/book_publishing_app/ui/shared/ErrorMapper.kt
+++ app/src/main/java/org/cescfe/book_publishing_app/ui/shared/ErrorMapper.kt
@@ -9,6 +9,7 @@ fun DomainErrorType.toStringResId(): Int = when (this) {
     DomainErrorType.TIMEOUT -> R.string.error_timeout
     DomainErrorType.NETWORK_ERROR -> R.string.error_network
     DomainErrorType.UNAUTHORIZED -> R.string.error_unauthorized
+    DomainErrorType.FORBIDDEN -> R.string.error_forbidden
     DomainErrorType.SERVER_ERROR -> R.string.error_server
     DomainErrorType.UNKNOWN -> R.string.error_unknown
 }
