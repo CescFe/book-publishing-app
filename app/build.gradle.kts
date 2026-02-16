339d4685924561ba13f24520e48686ecd4492813
FrancescFe <fraferru@gmail.com>
2026-02-16 12:07:47 +0100

    Feat(APP-079): increse app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.cescfe.book_publishing_app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
